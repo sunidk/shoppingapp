803228987c794b7e4656fbeb3f2d7413d95e9e57
SUNIL <sunidk97@gmail.com>
2022-09-06 23:07:44 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Navbar/>
+        <Home/>
         <Routes>
           <Route path="/" element={<Home/>}/>
           <Route path="/products" element={<Products/>}/>
